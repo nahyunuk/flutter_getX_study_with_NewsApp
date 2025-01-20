2ac19bb104dea2049b41cf532e3c83e280849c8e
Nahyunuk <s24022@gsm.hs.kr>
2025-01-20 14:49:09 +0900

    feat: main

--- lib/view/main_screen.dart
+++ lib/view/main_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mvvm_getx_news_app/dataSource/dataSource.dart';
 import 'package:mvvm_getx_news_app/repository/repository.dart';
+import 'package:mvvm_getx_news_app/view/detail_screen.dart';
 import '../viewModel/viewModel.dart';
 
 class MainScreen extends StatelessWidget {
@@ -12,27 +13,50 @@ class MainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        actions: [],
-      ),
-      body: Obx(
-        () {
-          if (viewModel.isLoading.value) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-          return ListView.builder(
-            itemCount: viewModel.news.length,
-            itemBuilder: (context, index) {
-              final news = viewModel.news[index];
-              return ListTile(
-                title: Text(news.title ?? 'No Title'),
-              );
-            },
-          );
-        },
+      resizeToAvoidBottomInset: false,
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(
+                vertical: size.height * 0.05, horizontal: size.width * 0.1),
+            child: SearchBar(
+              leading: Icon(Icons.search),
+              onSubmitted: (value) {
+                viewModel.fetchNews(value);
+              },
+            ),
+          ),
+          SizedBox(
+            height: size.height * 0.8,
+            child: Obx(
+              () {
+                if (viewModel.isLoading.value) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                return ListView.builder(
+                  itemCount: viewModel.news.length,
+                  itemBuilder: (context, index) {
+                    final news = viewModel.news[index];
+                    return GestureDetector(
+                      onTap: () {
+                        Get.to(()=>DetailScreen(index: index));
+                      },
+                      child: Card(
+                        child: ListTile(
+                          title: Text(news.title ?? 'No Title'),
+                        ),
+                      ),
+                    );
+                  },
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
