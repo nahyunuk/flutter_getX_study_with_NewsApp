24066b612bbd79fd893c463a44ee824d6e993ffb
Nahyunuk <s24022@gsm.hs.kr>
2025-01-20 11:37:03 +0900

    feat:뷰 모델 정의

---
+++ lib/viewModel/viewModel.dart
@@ -0,0 +1,32 @@
+import 'package:get/get.dart';
+import '../model/newsModel.dart';
+import '../repository/repository.dart';
+
+class NewsViewModel extends GetxController {
+  late final NewsArticleRepository repository;
+
+  NewsViewModel({required this.repository});
+
+  var news = <NewsArticle>[].obs;
+  var isLoading = true.obs;
+
+  @override
+  void onInit(){
+    super.onInit();
+    fetchNews();
+  }
+
+ Future<void> fetchNews() async {
+    try{
+      isLoading.value = true;
+      final fetchedNews = await repository.getNewsArticle();
+      news.value = fetchedNews;
+      print(fetchedNews);
+    } catch (e){
+      Get.snackbar('Error', 'Failed to load News : $e');
+    }
+    finally {
+      isLoading.value = false;
+    }
+ }
+}
